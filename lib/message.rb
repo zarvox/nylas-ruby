aea96fd502ce31f706e787737316d3804545ddb8
karim hamidou <khamidou@gmail.com>
2014-11-12 18:57:05 +0100

    WIP

--- lib/message.rb
+++ lib/message.rb
@@ -1,5 +1,6 @@
 require 'restful_model'
 require 'file'
+require 'rfc2882'
 
 module Inbox
   class Message < RestfulModel
@@ -26,6 +27,11 @@ module Inbox
 
     def files
       @files ||= RestfulModelCollection.new(File, @_api, @namespace_id, {:message_id=>@id})
+    end
+
+    def rfc2822
+      collection = RestfulModelCollection.new(Rfc2822, @_api, @namespace_id, {:message_id=>@id})
+      @rfc2822 ||= collection.get_model("rfc2822")
     end
 
   end
